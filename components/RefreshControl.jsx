31e864dabca02cd9767e540e1dc43484b1d63d8b
boris <yuhaoboris@gmail.com>
2024-06-30 12:02:27 +0800

    create home page and play videos

---
+++ components/RefreshControl.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RefreshControl = () => {
+  return (
+    <View>
+      <Text>RefreshControl</Text>
+    </View>
+  )
+}
+
+export default RefreshControl
